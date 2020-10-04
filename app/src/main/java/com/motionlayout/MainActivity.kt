f45e6ea25b8d037463ab69fa3a9ec60c222d71b0
Aadit Naik <cnaique@gmail.com>
2020-10-04 13:45:01 +0530

    learnt about motionalyout transition, constrain set setting & transform views

---
+++ app/src/main/java/com/motionlayout/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.motionlayout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_home, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_transform -> startActivity(Intent(this, MotionExpOne::class.java))
+            else -> {
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
---
+++ app/src/main/java/com/motionlayout/MotionExpOne.kt
@@ -0,0 +1,11 @@
+package com.motionlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MotionExpOne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion_exp_one)
+    }
+}
